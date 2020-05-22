627df741181f150790c9441c7f0695fbfc881c95
utrodus <come.programmer@gmail.com>
2020-05-22 22:43:57 +0700

    add styling with shadowdom

--- src/script/component/club-list.js
+++ src/script/component/club-list.js
@@ -2,23 +2,39 @@ import "./club-item.js";
 
 class ClubList extends HTMLElement {
 
+    constructor(){
+        super();
+        this.shadowDom = this.attachShadow({mode:"open"});
+    }
+
     set clubs(club) {
         this._clubs = club;
         this.render();
     }
 
     render() {
-        this.innerHTML = "";
+        this.shadowDom.innerHTML = "";
         this._clubs.forEach(club => {
             const clubItemElement = document.createElement("club-item");
             clubItemElement.club = club;
-            this.appendChild(clubItemElement);
+            this.shadowDom.appendChild(clubItemElement);
         })
     }
 
     renderError() {
-        this.innerHTML = "";
-        this.innerHTML += `<h2 class="placeholder">${message}</h2>`;
+        this.shadowDom.innerHTML = `
+        
+
+        .placeholder {
+            font-weight: lighter;
+            color: rgba(0, 0, 0, 0.5);
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+        `;
+        this.shadowDom.innerHTML += `<h2 class="placeholder">${message}</h2>`;
     }
 
 }
